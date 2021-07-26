15755350e0803bea21eebbdbfc24e8e75ee0df99
Noorudinz <noorudeens@yahoo.in>
2021-07-26 18:32:00 +0530

    Js interop with js file

---
+++ Syncfusion/DataGrid/SerAppDataGrid/SerAppDataGrid/wwwroot/js/JSInteroperable.js
@@ -0,0 +1,11 @@
+﻿function createAlert() {
+    alert("Hey this is an alert");
+}
+
+function createPrompt(question) {
+    return prompt(question);
+}
+
+function setElement(id, text) {
+    document.getElementById(id).innerText = text;
+}
