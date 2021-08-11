025379deb037426cca80ae891fd17ca46518dba9
Noorudinz <noorudeens@yahoo.in>
2021-08-11 11:26:20 +0530

    In memory cache implemented on Fetch weather data

--- Syncfusion/DataGrid/SerAppDataGrid/SerAppDataGrid/Data/WeatherForecastService.cs
+++ Syncfusion/DataGrid/SerAppDataGrid/SerAppDataGrid/Data/WeatherForecastService.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,15 +14,39 @@ namespace SerAppDataGrid.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
-        }
+        //[Inject]
+        //private IMemoryCache memoryCache { get; set; }
+        //public IEnumerable<WeatherForecast> GetForecastAsync(DateTime dtime)
+        //{
+        //    if (!memoryCache.TryGetValue("weatherdata", out IEnumerable<WeatherForecast> data))
+        //    {
+        //        var rng = new Random();
+        //        //return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //        //{
+        //        //    Date = startDate.AddDays(index),
+        //        //    TemperatureC = rng.Next(-20, 55),
+        //        //    Summary = Summaries[rng.Next(Summaries.Length)]
+        //        //}).ToArray());
+        //        data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //        {
+        //            Date = DateTime.Now.AddDays(index),
+        //            TemperatureC = rng.Next(-20, 55),
+        //            Summary = Summaries[rng.Next(Summaries.Length)]
+        //        }).ToArray();
+
+        //        var cacheOption = new MemoryCacheEntryOptions()
+        //        {
+        //            Priority = CacheItemPriority.High,
+        //            AbsoluteExpiration = DateTime.Now.AddMinutes(10),
+        //            Size = 128
+        //        };
+
+        //        memoryCache.Set("weatherdata", data, cacheOption);
+        //    }
+
+        //    return data;
+
+        //}
+
     }
 }
--- Syncfusion/DataGrid/SerAppDataGrid/SerAppDataGrid/Pages/FetchDataBase.cs
+++ Syncfusion/DataGrid/SerAppDataGrid/SerAppDataGrid/Pages/FetchDataBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
 using SerAppDataGrid.Data;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,54 @@ namespace SerAppDataGrid.Pages
 {
     public class FetchDataBase : ComponentBase
     {
-        protected WeatherForecast[] forecasts;
+        protected IEnumerable<WeatherForecast> forecasts;
 
         [Inject]  
         private WeatherForecastService ForecastServicee { get; set; }
+
+        [Inject]
+        private IMemoryCache memoryCache { get; set; }
         protected override async Task OnInitializedAsync()
         {
-            forecasts = await ForecastServicee.GetForecastAsync(DateTime.Now);
+             forecasts = GetForecastAsync(DateTime.Now);
+       
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> GetForecastAsync(DateTime dtime)
+        {
+            if (!memoryCache.TryGetValue("weatherdata", out IEnumerable<WeatherForecast> data))
+            {
+                var rng = new Random();
+                //return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                //{
+                //    Date = startDate.AddDays(index),
+                //    TemperatureC = rng.Next(-20, 55),
+                //    Summary = Summaries[rng.Next(Summaries.Length)]
+                //}).ToArray());
+                data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                }).ToArray();
+
+                var cacheOption = new MemoryCacheEntryOptions()
+                {
+                    Priority = CacheItemPriority.High,
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(10),
+                    Size = 128
+                };
+
+                memoryCache.Set("weatherdata", data, cacheOption);
+            }
+
+            return data;
+
         }
     }
 }
