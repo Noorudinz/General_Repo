7974044139144d994e14610003ca595f2f4b6075
Noorudins <noorudins@cloudix.io>
2022-08-08 20:03:35 +0530

    Console App POD Init

---
+++ POD/ConsoleAppPOD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppPOD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
