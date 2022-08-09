c18de4ddc09e96d79f2de98b1c310538b9a47a67
Noorudins <noorudins@cloudix.io>
2022-08-09 09:09:05 +0530

    Day 2 - find out missing number from array

---
+++ POD/ConsoleAppPOD/Missing_number_from_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD
+{
+    public class Missing_number_from_array
+    {
+        public static void missingNumber()
+        {
+            int[] arr = { 111, 112, 115, 117, 114, 113 };
+
+            int n = arr.Length;
+
+            //formula for find sum of sequence including missing number
+            int total = ((n + 1) * (n + 2)) / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                total -= arr[i];
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
