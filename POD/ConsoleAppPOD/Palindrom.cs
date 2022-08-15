3ee71f0d89ffefcea2d5fd505edbfc6a58e4de50
Noorudins <noorudins@cloudix.io>
2022-08-15 13:35:47 +0530

    Day 6 - Palindrome using numbers

---
+++ POD/ConsoleAppPOD/Palindrom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD
+{
+    public class Palindrom
+    {
+        //Palindrome function using numbers
+        public static void PalindromNumbers()
+        {
+            int number = 123291;
+            int remainder, sum = 0;
+            int temp = number;
+
+            while (number > 0)
+            {
+                remainder = number % 10;
+
+                sum = (sum * 10) + remainder;
+
+                number = number / 10;
+            }
+
+            if (temp == sum)
+                Console.WriteLine($"Number {temp} is Palindrome.");
+            else
+                Console.WriteLine($"Number {temp} is not Palindrome.");
+        }
+
+        //Palindrome function using strings
+        public static void PalindromStrings()
+        {
+
+        }
+
+    }
+}
