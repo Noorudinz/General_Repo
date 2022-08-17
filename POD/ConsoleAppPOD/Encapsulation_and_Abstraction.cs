a0e3ae4285bbe4836e9c7e43a23401e283f4f423
Noorudins <noorudins@cloudix.io>
2022-08-17 18:35:28 +0530

    Day 7 - Difference encapsulation and abstraction

---
+++ POD/ConsoleAppPOD/Encapsulation_and_Abstraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD
+{
+    class Employee
+    {
+        //When we allow the function CheckEmployee to be accessed from the outside world and hide the internal processes
+        //like IsEmployeeExist and DeleteEmployee as an abstraction.
+        public void CheckEmployee(int Id)
+        {
+
+            IsEmployeeExist(1);
+            DeleteEmployee(2);
+        }
+
+        //When we convert the functions IsEmployeeExist and DeleteEmployee access modifiers to private,
+        //then we have done encapsulation.
+
+        private bool IsEmployeeExist(int Id)
+        {
+            return true;
+        }
+
+        private bool DeleteEmployee(int Id)
+        {
+            return true;
+        }
+    }
+
+    class Encapsulation_and_Abstraction
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.CheckEmployee(1);
+        }
+
+    }
+}
