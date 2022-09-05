1c70c218ae2dc551979f6b54231530ed1594fe34
Noorudins <noorudins@cloudix.io>
2022-09-05 08:34:04 +0530

    modified

--- POD/ConsoleAppPOD/ArrayProgram/Minimum_Maximum.cs
+++ POD/ConsoleAppPOD/ArrayProgram/Minimum_Maximum.cs
@@ -8,23 +8,27 @@ namespace ConsoleAppPOD.ArrayProgram
 {
     public class Minimum_Maximum
     {
-        int[] arr = { 25, 45, 96, 35, 52, 75 };
-        int n = arr.Length;
-        int min, max;
+        public static void MainProgram()
+        {
+            int[] arr = { 25, 45, 96, 35, 52, 75 };
+            int n = arr.Length;
+            int min, max;
 
-        max = arr[0];   
+            max = arr[0];
             min = arr[0];
 
-            for(int i = 1; i<n; i++)
+            for (int i = 1; i < n; i++)
             {
-                if(arr[i] > max)
+                if (arr[i] > max)
                     max = arr[i];
 
-                if(arr[i] < min)
+                if (arr[i] < min)
                     min = arr[i];
             }
 
-    Console.WriteLine("Minimum element : " + min);
+            Console.WriteLine("Minimum element : " + min);
             Console.WriteLine("Maximun element : " + max);
+        }
+     
     }
 }
