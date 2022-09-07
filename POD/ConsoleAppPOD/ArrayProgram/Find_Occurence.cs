3d059e99f2a1d738c6f68ba3696ebe96b4699747
Noorudins <noorudins@cloudix.io>
2022-09-07 08:33:18 +0530

    Day 17 - Count number of occurence of element in given an array

---
+++ POD/ConsoleAppPOD/ArrayProgram/Find_Occurence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD.ArrayProgram
+{
+    public class Find_Occurence
+    {
+        static void MainProgram()
+        {
+            int[] arr = { 1, 1, 2, 2, 2, 3, 3, 3, 3, 4 };
+            int n = arr.Length;
+            int x = 2;
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (x == arr[i])
+                    count++;
+            }
+
+            Console.WriteLine("Occurence of {0} is : {1}", x, count);
+        }
+    }
+}
