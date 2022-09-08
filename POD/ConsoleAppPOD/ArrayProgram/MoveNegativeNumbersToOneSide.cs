b8cf2541ab7c8275679d94bc3e0cdcfc2cde8090
Noorudins <noorudins@cloudix.io>
2022-09-08 08:28:43 +0530

    Day 18 - move negative element to one side of array

---
+++ POD/ConsoleAppPOD/ArrayProgram/MoveNegativeNumbersToOneSide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD.ArrayProgram
+{
+    public class MoveNegativeNumbersToOneSide
+    {
+        static void MainProgram()
+        {
+            int[] arr = { 12, -56, 41, -75, 36, -96, 85, -25 };
+            Array.Sort(arr);
+
+            foreach (int i in arr)
+                Console.Write(i + " ");
+        }
+    }
+}
