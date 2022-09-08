078219092f3d21b5385c02854df0545bf4f33efb
Noorudins <noorudins@cloudix.io>
2022-09-08 08:45:57 +0530

    Day 18 - Union and intersect of two given arrays

---
+++ POD/ConsoleAppPOD/ArrayProgram/Union_Intersect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOD.ArrayProgram
+{
+    public class Union_Intersect
+    {
+        static void MainProgram()
+        {
+            int[] arr1 = { 2, 5, 8, 7, 10 };
+            int[] arr2 = { 1, 5, 3, 2, 9 };
+
+            var union = arr1.Union(arr2).ToArray();
+            var intersect = arr1.Intersect(arr2).ToArray();
+
+            Console.WriteLine(union);
+            Console.WriteLine(intersect);
+        }
+    }
+}
